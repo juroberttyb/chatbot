2d1058386f8d93fdb750d9ba1b1ff7514b2a3905
root <juroberttyb@gmail.com>
2022-01-04 06:10:18 +0000

    [DEVEL] 1:interactive msg exchange between server and client [TODO] 1:input at client side will be interrupted by server side msg, this is currently not dealt with to focus on fast prototyping of whole online AI chatbot project.

--- client.py
+++ client.py
@@ -1,30 +1,68 @@
-import socket
+import socket, threading
+
+"""keep input at bottom without getting interrupted by server side input"""
+"""should add unit test for functionality testing"""
 
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = "172.17.0.2"
 ADDR = (SERVER, PORT)
 SIZE = 4
+#title = "Me"
+#buf_curidx, max_buf = 1, 7
+#buffer = [""] * 6 + ["--- Welcome to Robert's Chatbot, you can now chat, or simply press enter to exit, thank you ---"]
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
+def display():
+    import time
+    while True:
+        time.sleep(1.0)
+        print("testing")
+
+def server_msg_handler():
+    while True:
+        buflen = client.recv(SIZE).decode(FORMAT)
+
+        try:
+            buflen = int(buflen)
+        except:
+            break
+        
+        if buflen > 8192 or buflen == 0:
+            break
+
+        msg = client.recv(buflen).decode(FORMAT)
+        print(f"Robert:{msg}")
+        # buffer.append(f"Robert:{msg}")
+    client.close()
+
 def send(msg):
+    try:
+        buflen = str(len(msg))
+        buflen = b'0' * (SIZE-len(buflen)) + buflen.encode(FORMAT)
+        client.send(buflen)
+        client.send(msg.encode(FORMAT))
+        #buffer.append(f"{title}:{msg}")
+        return False
+    except:
+        return True
+
+# threading.Thread(target=display).start()
+threading.Thread(target=server_msg_handler).start()
+
+print("--- Welcome to Robert's Chatbot, you can now chat, or simply press enter to exit, thank you ---")
+while True:
+    # msg = input(f"{title}:")
+    msg = input()
     if len(msg) == 0:
         ret = input("Sure you want to exit? [y|n]:")
         if ret == 'y':
             client.send(b'0' * SIZE)
-            exit()
-        return
+            exit("chatbot is closed")
 
-    buflen = str(len(msg))
-    buflen = b'0' * (SIZE-len(buflen)) + buflen.encode(FORMAT)
-    client.send(buflen)
-    client.send(msg.encode(FORMAT))
-
-print("--- Welcome to Robert's Chatbot, you can now chat, or simply press enter to exit, thank you ---")
-while True:
-    msg = input("Me:")
-    send(msg)
+    """if something went wrong in sending msg"""
+    if send(msg):
+        exit("connection failed")
