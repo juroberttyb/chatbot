1ed70e0a19463d435928abb3bb61d8b64b064870
root <juroberttyb@gmail.com>
2022-01-16 14:05:29 +0000

    [DEVEL] 1:pretrained bert model added for better performace

--- chatter.py
+++ chatter.py
@@ -4,6 +4,7 @@ def train_new_model():
     os.system('mkdir -p new_model')
 
     from parlai.scripts.train_model import TrainModel
+    """
     TrainModel.main(
         # we MUST provide a filename
         model_file='new_model/model',
@@ -24,9 +25,87 @@ def train_new_model():
         # truncate text and labels at 64 tokens, for memory and time savings
         truncate=64,
     )
+    """
+
+    """
+    TrainModel.main(
+        # similar to before
+        task='empathetic_dialogues', 
+        model='transformer/generator',
+        model_file='new_model/model',
+        
+        # initialize with a pretrained model
+        # init_model='zoo:tutorial_transformer_generator/model',
+        
+        # arguments we get from the pretrained model.
+        # Unfortunately, these must be looked up separately for each model.
+        n_heads=8, n_layers=8, n_positions=512, text_truncate=512,
+        label_truncate=128, ffn_size=2048, embedding_size=256,
+        activation='gelu', variant='xlm',
+        dict_lower=True, dict_tokenizer='bpe',
+        dict_file='zoo:tutorial_transformer_generator/model.dict',
+        learn_positional_embeddings=True,
+        
+        # some training arguments, specific to this fine-tuning
+        # use a small learning rate with ADAM optimizer
+        lr=1e-5, optimizer='adam',
+        warmup_updates=100,
+        # early stopping on perplexity
+        validation_metric='ppl',
+        # train at most x seconds, and validate every 0.25 epochs
+        max_train_time=6*60*60, validation_every_n_epochs=1.0,
+        
+        # depend on your gpu. If you have a V100, this is good
+        batchsize=12, fp16=True, fp16_impl='mem_efficient',
+        
+        # speeds up validation
+        skip_generation=True,
+        
+        # helps us cram more examples into our gpu at a time
+        dynamic_batching='full',
+    )
+    """
+
+    TrainModel.main(
+        # similar to before
+        task='empathetic_dialogues', 
+        model='transformer/generator',
+        model_file='new_model/model',
+        
+        # initialize with a pretrained model
+        init_model='zoo:tutorial_transformer_generator/model',
+        
+        # arguments we get from the pretrained model.
+        # Unfortunately, these must be looked up separately for each model.
+        n_heads=16, n_layers=8, n_positions=512, text_truncate=512,
+        label_truncate=128, ffn_size=2048, embedding_size=512,
+        activation='gelu', variant='xlm',
+        dict_lower=True, dict_tokenizer='bpe',
+        dict_file='zoo:tutorial_transformer_generator/model.dict',
+        learn_positional_embeddings=True,
+        
+        # some training arguments, specific to this fine-tuning
+        # use a small learning rate with ADAM optimizer
+        lr=1e-5, optimizer='adam',
+        warmup_updates=100,
+        # early stopping on perplexity
+        validation_metric='ppl',
+        # train at most 10 minutes, and validate every 0.25 epochs
+        max_train_time=60, validation_every_n_epochs=0.25,
+        
+        # depend on your gpu. If you have a V100, this is good
+        batchsize=10, fp16=True, fp16_impl='mem_efficient',
+        
+        # speeds up validation
+        skip_generation=True,
+        
+        # helps us cram more examples into our gpu at a time
+        dynamic_batching='full',
+    )
 
     os.system('rm -rf model')
     os.system("mv new_model model")
+    os.system('rm -rf new_model')
 
 from parlai.core.teachers import register_teacher, DialogTeacher
 @register_teacher("message")
